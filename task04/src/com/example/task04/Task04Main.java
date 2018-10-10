09fd7f56e5cc238b4a157263157cfb05b0654215
BMSalamander <hostan3@gmail.com>
2018-10-10 19:26:22 +0500

    Антипьев Александр. Задание 04.

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -3,5 +3,17 @@ package com.example.task04;
 public class Task04Main {
     public static void main(String[] args) {
 
+        Line l1 = new Line(new Point(2,4), new Point(8, 9));
+        Point p = new Point(4, 6);
+        Point colPoint = new Point(2, 4);
+
+        if (l1.isCollinearLine(p)) {
+            System.out.println("p is here!");
+        }
+
+        if (l1.isCollinearLine(colPoint)) {
+            System.out.println("colPoint is really co(o)l");
+        }
+
     }
 }
