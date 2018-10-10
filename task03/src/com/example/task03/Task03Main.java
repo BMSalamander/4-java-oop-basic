ca8e2996e4ebda83aab750c1a524cdb43529af6d
BMSalamander <hostan3@gmail.com>
2018-10-10 11:01:00 +0500

    Антипьев Александр. Задание 03.

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,13 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
+        ComplexNumber n1 = new ComplexNumber(2.2, 5.5);
+        ComplexNumber n2 = new ComplexNumber(7.2, 8.1);
+        System.out.println(n1);
+        System.out.println(n2);
+
+        System.out.println(n1.add(n2));
+        System.out.println(n1.mult(n2));
 
     }
 }
