55bd23245efef58004e118d3b58d6ddcb3204984
BMSalamander <hostan3@gmail.com>
2018-10-10 10:41:33 +0500

    Антипьев Александр. Задание 02.

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -2,6 +2,19 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
+        TimeSpan time1 = new TimeSpan(4, 20, 66);
+        System.out.println("The time1 is: " + time1);
 
+        TimeSpan time2 = new TimeSpan(22, 78, 99);
+        System.out.println("The time2 is: " + time2);
+
+        time1.add(time2);
+        System.out.println("Time1 + Time2: " + time1);
+
+        time1.subtract(time2);
+        System.out.println("Time1 + Time2 - Time2: " + time1);
+
+        time2.subtract(time1);
+        System.out.println("Time2 - Time1: " + time2);
     }
 }
